4c77ebd9ed63d0fca250d8a04613682e8399ee2b
zoki <zoran.smilevski@biokoda.com>
2024-12-06 10:00:04 +0100

    *day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,61 +1,87 @@
+import Direction.DOWN
+import Direction.LEFT
+import Direction.RIGHT
+import Direction.UP
+import java.awt.Point
+import kotlin.lazy
+
 private const val DAY = "Day06"
 
 private data class GuardPosition(
     val x: Int,
     val y: Int,
     val maxX: Int,
-    val maxY: Int
+    val maxY: Int,
+    val direction: Direction
 ) {
+    val point by lazy { Point(x, y) }
+
     fun isInside(): Boolean = x > 0 && x < maxX && y > 0 && y < maxY
+
+    fun makeMove() = when (direction) {
+        UP -> copy(y = y - 1)
+        DOWN -> copy(y = y + 1)
+        LEFT -> copy(x = x - 1)
+        RIGHT -> copy(x = x + 1)
+    }
+
+    fun changeDirection() = when (direction) {
+        UP -> copy(direction = RIGHT)
+        DOWN -> copy(direction = LEFT)
+        LEFT -> copy(direction = UP)
+        RIGHT -> copy(direction = DOWN)
+    }
 }
 
 private enum class Direction {
     UP,
     DOWN,
     LEFT,
-    RIGHT;
-
-    fun makeMove(position: GuardPosition) = when (this) {
-        UP -> position.copy(y = position.y - 1)
-        DOWN -> position.copy(y = position.y + 1)
-        LEFT -> position.copy(x = position.x - 1)
-        RIGHT -> position.copy(x = position.x + 1)
-    }
-
-    fun changeDirection() = when (this) {
-        UP -> RIGHT
-        DOWN -> LEFT
-        LEFT -> UP
-        RIGHT -> DOWN
-    }
+    RIGHT
 }
 
 fun main() {
 
-    class LabMap(input: List<String>) {
+    class LabMap(input: List<String>, modifyX: Int? = null, modifyY: Int? = null) {
         val data = input.map { it.toCharArray() }.toTypedArray()
         val visited = mutableSetOf<GuardPosition>()
-
         var guardPosition: GuardPosition = run {
             data.forEachIndexed { y, line ->
                 line.forEachIndexed { x, item ->
                     if (item == '^') {
-                        return@run GuardPosition(x, y, line.lastIndex, data.lastIndex)
+                        return@run GuardPosition(x, y, line.lastIndex, data.lastIndex, UP)
                     }
                 }
             }
             throw IllegalStateException("Guard position not found")
         }
         var steps = 0
-        var direction = Direction.UP
 
-        fun getNextPosition(): GuardPosition {
-            while (true) {
-                val tryMove = direction.makeMove(guardPosition)
-                if (data[tryMove.y][tryMove.x] == '#') {
-                    direction = direction.changeDirection()
+        init {
+            if (modifyX != null && modifyY != null) {
+                val current = data[modifyY][modifyX]
+                if (current == '#' || current == '^') {
+                    throw IllegalStateException("Can't modify")
                 } else {
-                    return tryMove
+                    data[modifyY][modifyX] = '#'
+                }
+            }
+        }
+
+        fun getNextPosition(): GuardPosition? {
+            while (true) {
+                val tryMove = guardPosition.makeMove()
+                when {
+                    data[tryMove.y][tryMove.x] == '#' -> {
+                        guardPosition = guardPosition.changeDirection()
+                    }
+                    visited.contains(tryMove) -> {
+                        return null
+                    }
+                    else -> {
+                        visited.add(tryMove)
+                        return tryMove
+                    }
                 }
             }
         }
@@ -63,12 +89,19 @@ fun main() {
         fun findExit(): Int {
 //            println("$steps. Guard move: $guardPosition")
             while (guardPosition.isInside()) {
-                guardPosition = getNextPosition()
-                visited.add(guardPosition)
+                guardPosition = getNextPosition() ?: return -1 // loop
                 steps++
 //                println("$steps. Guard move: $guardPosition")
             }
-            return visited.size
+            return visited.distinctBy { position -> position.point }.size
+        }
+
+        fun findLoop(): Boolean {
+            while (guardPosition.isInside()) {
+                guardPosition = getNextPosition() ?: return true
+            }
+
+            return false
         }
     }
 
@@ -78,14 +111,24 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        return 0
+        var loops = 0
+        input.forEachIndexed { y, line ->
+            line.forEachIndexed { x, item ->
+                val map = runCatching { LabMap(input, x, y) }.getOrNull()
+                val hasLoop = map?.findLoop() == true
+                if (hasLoop) {
+                    loops++
+                }
+            }
+        }
+        return loops
     }
 
     val testInput = readInput("${DAY}_test")
     check(part1(testInput) == 41)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 6)
 
     val input = readInput(DAY)
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
