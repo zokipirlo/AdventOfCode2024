8d60ee6608c40a84a4ecc7c0dab65cd2a21a0652
zoki <zoran.smilevski@biokoda.com>
2024-12-06 08:48:30 +0100

    *wrap in class so it's not global

--- src/Day05.kt
+++ src/Day05.kt
@@ -6,94 +6,94 @@ fun main() {
         val after: MutableList<Int> = mutableListOf()
     )
 
-    val instructions = mutableListOf<List<Int>>()
-    val orderMap = mutableMapOf<Int, OrderRule>()
+    class PrintQueue {
+        val instructions = mutableListOf<List<Int>>()
+        val orderMap = mutableMapOf<Int, OrderRule>()
 
-    fun parseRule(rule: String) {
-        val (before, after) = rule.split("|").map { string -> string.toInt() }
-        orderMap.getOrPut(before) { OrderRule() }.also { rule ->
-            rule.after.add(after)
-        }
-        orderMap.getOrPut(after) { OrderRule() }.also { rule ->
-            rule.before.add(before)
-        }
-    }
-
-    fun parseInstruction(string: String) {
-        instructions.add(string.split(",").map { it.toInt() })
-    }
-
-    fun parseData(input: List<String>) {
-        instructions.clear()
-        orderMap.clear()
-
-        var parseRules = true
-        input.forEach { line ->
-            if (line.isEmpty()) {
-                parseRules = false
-                return@forEach
+        fun parseRule(rule: String) {
+            val (before, after) = rule.split("|").map { string -> string.toInt() }
+            orderMap.getOrPut(before) { OrderRule() }.also { rule ->
+                rule.after.add(after)
             }
-            when (parseRules) {
-                true -> parseRule(line)
-                false -> parseInstruction(line)
+            orderMap.getOrPut(after) { OrderRule() }.also { rule ->
+                rule.before.add(before)
             }
         }
-    }
 
-    fun validateInstructionItem(instruction: List<Int>, index: Int): Int {
-        val num = instruction[index]
+        fun parseInstruction(string: String) {
+            instructions.add(string.split(",").map { it.toInt() })
+        }
 
-        val rules = orderMap[num]
-        if (rules == null) {
+        fun parseData(input: List<String>) {
+            var parseRules = true
+            input.forEach { line ->
+                if (line.isEmpty()) {
+                    parseRules = false
+                    return@forEach
+                }
+                when (parseRules) {
+                    true -> parseRule(line)
+                    false -> parseInstruction(line)
+                }
+            }
+        }
+
+        fun validateInstructionItem(instruction: List<Int>, index: Int): Int {
+            val num = instruction[index]
+
+            val rules = orderMap[num]
+            if (rules == null) {
+                return -1
+            }
+
+            val beforeNums = instruction.subList(0, index)
+            val afterNums = instruction.subList(index + 1, instruction.size)
+
+            val errorBefore = beforeNums.find { before -> rules.after.contains(before) }
+            if (errorBefore != null) {
+//            println("error rule $num|$errorBefore")
+                return instruction.indexOf(errorBefore)
+            }
+
+            val errorAfter = afterNums.find { after -> rules.before.contains(after) }
+            if (errorAfter != null) {
+//            println("error rule errorAfter|$num")
+                return instruction.indexOf(errorAfter)
+            }
+
             return -1
         }
 
-        val beforeNums = instruction.subList(0, index)
-        val afterNums = instruction.subList(index + 1, instruction.size)
-
-        val errorBefore = beforeNums.find { before -> rules.after.contains(before) }
-        if (errorBefore != null) {
-//            println("error rule $num|$errorBefore")
-            return instruction.indexOf(errorBefore)
-        }
-
-        val errorAfter = afterNums.find { after -> rules.before.contains(after) }
-        if (errorAfter != null) {
-//            println("error rule errorAfter|$num")
-            return instruction.indexOf(errorAfter)
-        }
-
-        return -1
-    }
-
-    fun validateInstruction(instruction: List<Int>): Boolean {
-        return instruction.indices.all { index ->
-            validateInstructionItem(instruction, index) == -1
-        }
-    }
-
-    fun fixInstruction(instruction: List<Int>): List<Int> {
-        val results = instruction.indices.map { index ->
-            index to validateInstructionItem(instruction, index)
-        }
-        val firstError = results.firstOrNull { pair -> pair.second != -1 }
-
-        if (firstError != null) {
-            val item = instruction[firstError.second]
-            val fixed = instruction.toMutableList().apply {
-                removeAt(firstError.second)
-                add(firstError.first, item)
+        fun validateInstruction(instruction: List<Int>): Boolean {
+            return instruction.indices.all { index ->
+                validateInstructionItem(instruction, index) == -1
             }
-            return fixInstruction(fixed)
-        } else {
-            return instruction
+        }
+
+        fun fixInstruction(instruction: List<Int>): List<Int> {
+            val results = instruction.indices.map { index ->
+                index to validateInstructionItem(instruction, index)
+            }
+            val firstError = results.firstOrNull { pair -> pair.second != -1 }
+
+            if (firstError != null) {
+                val item = instruction[firstError.second]
+                val fixed = instruction.toMutableList().apply {
+                    removeAt(firstError.second)
+                    add(firstError.first, item)
+                }
+                return fixInstruction(fixed)
+            } else {
+                return instruction
+            }
         }
     }
 
     fun part1(input: List<String>): Int {
-        parseData(input)
-        return instructions.filter {
-            validateInstruction(it)
+        val printQueue = PrintQueue()
+        printQueue.parseData(input)
+        return printQueue.instructions.filter {
+            printQueue.validateInstruction(it)
         }.sumOf { valid ->
             valid[valid.size / 2]
         }
@@ -101,11 +101,12 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        parseData(input)
-        return instructions.filterNot {
-            validateInstruction(it)
+        val printQueue = PrintQueue()
+        printQueue.parseData(input)
+        return printQueue.instructions.filterNot {
+            printQueue.validateInstruction(it)
         }.map { problematic ->
-            fixInstruction(problematic)
+            printQueue.fixInstruction(problematic)
         }.sumOf { valid ->
             valid[valid.size / 2]
         }
