98b057d907dd49fbe1f72eb72f31aba9f78c4a97
zoki <zoran.smilevski@biokoda.com>
2024-12-08 12:33:33 +0100

    *day template

---
+++ src/DayTemplate.kt
@@ -0,0 +1,20 @@
+private const val DAY = "Day__"
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("${DAY}_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput(DAY)
+    part1(input).println()
+    part2(input).println()
+}
